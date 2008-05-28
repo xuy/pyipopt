4f5b64f2a740aedd35e5c6bc2fc7ef3cb11b0b0f
Eric Xu <xu.mathena@gmail.com>
2008-05-28 22:32:31 +0000

    Delete
    git-svn-id: https://pyipopt.googlecode.com/svn/trunk@25 818b84ff-3949-0410-bb35-eb5a4b088611

--- batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# To run the code, compile it with Python include file and ipopt library
-gcc -g -I/usr/local/include/python2.5 -I/Project/ThirdParty/ipopt/Ipopt-3.3.3/include/ipopt/ -I/usr/lib/python2.5/site-packages/numpy/core/include/ -fpic -shared -lipopt -lg2c -lm -o pyipopt.so pyipopt.c callback.c 
-# copy it to the python dir 
-# cp ./ipopt.so /usr/lib/python2.5/ 
-
-python amplipopt.py test.nl
-python example.py
-
