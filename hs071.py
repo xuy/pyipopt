11adb28a8b10c9b80e4d39aecd48e0399d2a23f5
Eric Xu <xu.mathena@gmail.com>
2012-03-01 22:25:37 -0600

    Revert "Revert "stuff""
    This reverts commit 50992fde41a4cd65df745bef0defd79e7f969e51.

--- hs071.py
+++ hs071.py
@@ -112,13 +112,18 @@ x, zl, zu, obj, status = nlp.solve(x0)
 # import pdb; pdb.set_trace()
 nlp.close()
 
+def print_variable(variable_name, value):
+  for i in xrange(len(value)):
+    print variable_name + "["+str(i)+"] =", value[i]
+
+print
 print "Solution of the primal variables, x"
-for i in xrange(len(x)):
-  print "x["+str(i)+"] =", x[i]
+print_variable("x", x)
 print
 print "Solution of the bound multipliers, z_L and z_U"
-print zl, zu
-
+print_variable("z_L", zl)
+print_variable("z_U", zu)
+print
 print "Objective value"
 print "f(x*) =", obj
 
