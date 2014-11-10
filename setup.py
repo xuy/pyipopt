8c0bf6f8577fb93193f2d4333a673835ecb5b384
brecht <brecht.baeten@gmail.com>
2014-11-10 08:18:02 +0100

    changed blas and lapack to coinblas and coinlapack as this is how they are defined in my version of ipopt

--- setup.py
+++ setup.py
@@ -48,11 +48,11 @@ pyipopt_extension = Extension(
         #extra_link_args=['-Wl,--rpath','-Wl,'+ IPOPT_LIB],
         library_dirs=[IPOPT_LIB],
         libraries=[
-            'ipopt', 'blas',
+            'ipopt', 'coinblas',
             #'coinhsl',
             'coinmumps',
             'coinmetis',
-            'lapack','dl','m',
+            'coinlapack','dl','m',
             ],
         include_dirs=[numpy_include, IPOPT_INC],
         )
