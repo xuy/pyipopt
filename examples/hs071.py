76d061cf0073e448537c6f9b7b58117eec536d0c
Gerhard Bräunlich <g.braeunlich@disroot.org>
2018-07-31 14:55:27 +0200

    examples/hs071.py: PEP8 compliance (formatting only)

--- examples/hs071.py
+++ examples/hs071.py
@@ -14,60 +14,69 @@ x_U = ones((nvar), dtype=float_) * 5.0
 ncon = 2
 
 g_L = array([25.0, 40.0])
-g_U = array([2.0*pow(10.0, 19), 40.0]) 
+g_U = array([2.0 * pow(10.0, 19), 40.0])
 
-def eval_f(x, user_data = None):
+
+def eval_f(x, user_data=None):
     assert len(x) == 4
     return x[0] * x[3] * (x[0] + x[1] + x[2]) + x[2]
 
-def eval_grad_f(x, user_data = None):
+
+def eval_grad_f(x, user_data=None):
     assert len(x) == 4
     grad_f = array([
-        x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]) , 
+        x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]),
         x[0] * x[3],
         x[0] * x[3] + 1.0,
         x[0] * (x[0] + x[1] + x[2])
-        ], float_)
-    return grad_f;
+    ], float_)
+    return grad_f
 
-def eval_g(x, user_data= None):
+
+def eval_g(x, user_data=None):
     assert len(x) == 4
     return array([
-        x[0] * x[1] * x[2] * x[3], 
-        x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3]
-        ], float_)
+        x[0] * x[1] * x[2] * x[3],
+        x[0] * x[0] + x[1] * x[1] + x[2] * x[2] + x[3] * x[3]
+    ], float_)
+
 
 nnzj = 8
-def eval_jac_g(x, flag, user_data = None):
+
+
+def eval_jac_g(x, flag, user_data=None):
     if flag:
         return (array([0, 0, 0, 0, 1, 1, 1, 1]),
                 array([0, 1, 2, 3, 0, 1, 2, 3]))
     else:
         assert len(x) == 4
-        return array([ x[1]*x[2]*x[3], 
-            x[0]*x[2]*x[3], 
-            x[0]*x[1]*x[3], 
-            x[0]*x[1]*x[2],
-            2.0*x[0], 
-            2.0*x[1], 
-            2.0*x[2], 
-            2.0*x[3] ])
+        return array([x[1] * x[2] * x[3],
+                      x[0] * x[2] * x[3],
+                      x[0] * x[1] * x[3],
+                      x[0] * x[1] * x[2],
+                      2.0 * x[0],
+                      2.0 * x[1],
+                      2.0 * x[2],
+                      2.0 * x[3]])
+
 
 nnzh = 10
-def eval_h(x, lagrange, obj_factor, flag, user_data = None):
+
+
+def eval_h(x, lagrange, obj_factor, flag, user_data=None):
     if flag:
         hrow = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3]
         hcol = [0, 0, 1, 0, 1, 2, 0, 1, 2, 3]
         return (array(hcol), array(hrow))
     else:
         values = zeros((10), float_)
-        values[0] = obj_factor * (2*x[3])
+        values[0] = obj_factor * (2 * x[3])
         values[1] = obj_factor * (x[3])
         values[2] = 0
         values[3] = obj_factor * (x[3])
         values[4] = 0
         values[5] = 0
-        values[6] = obj_factor * (2*x[0] + x[1] + x[2])
+        values[6] = obj_factor * (2 * x[0] + x[1] + x[2])
         values[7] = obj_factor * (x[0])
         values[8] = obj_factor * (x[0])
         values[9] = 0
@@ -85,10 +94,13 @@ def eval_h(x, lagrange, obj_factor, flag, user_data = None):
         values[9] += lagrange[1] * 2
         return values
 
+
 def apply_new(x):
     return True
 
-nlp = pyipopt.create(nvar, x_L, x_U, ncon, g_L, g_U, nnzj, nnzh, eval_f, eval_grad_f, eval_g, eval_jac_g)
+
+nlp = pyipopt.create(nvar, x_L, x_U, ncon, g_L, g_U, nnzj,
+                     nnzh, eval_f, eval_grad_f, eval_g, eval_jac_g)
 
 x0 = array([1.0, 5.0, 5.0, 1.0])
 pi0 = array([1.0, 1.0])
@@ -128,9 +140,11 @@ x, zl, zu, constraint_multipliers, obj, status = nlp.solve(x0)
 # import pdb; pdb.set_trace()
 nlp.close()
 
+
 def print_variable(variable_name, value):
     for i in range(len(value)):
-        print("{} {}".format(variable_name + "["+str(i)+"] =", value[i]))
+        print("{} {}".format(variable_name + "[" + str(i) + "] =", value[i]))
+
 
 print("Solution of the primal variables, x")
 print_variable("x", x)
@@ -144,4 +158,3 @@ print_variable("lambda", constraint_multipliers)
 
 print("Objective value")
 print("f(x*) = {}".format(obj))
-
