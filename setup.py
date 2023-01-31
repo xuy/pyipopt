c624b2238636c105afb9708567796392bf949e26
Eric Xu <1151720+xuy@users.noreply.github.com>
2023-01-30 21:41:37 -0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,72 +1,61 @@
-# Originally contributed by Lorne McIntosh.
-# Modified by Eric Xu
-# Further modification by random internet people.
-
-# You will probably have to edit this file in unpredictable ways
-# if you want pyipopt to work for you, sorry.
-
-# When I installed Ipopt from source, I used the
-# --prefix=/usr/local
-# option, so this is where I want pyipopt to look for my ipopt installation.
-# I only installed from source because the ipopt packaging
-# for my linux distribution was buggy,
-# so by the time you read this the bugs have probably been fixed
-# and you will want to specify a different directory here.
-IPOPT_DIR = '/usr/local/'
-
 import os
 from distutils.core import setup
 from distutils.extension import Extension
 
-# NumPy is much easier to install than pyipopt,
-# and is a pyipopt dependency, so require it here.
-# We need it to tell us where the numpy header files are.
-import numpy
-numpy_include = numpy.get_include()
+# IPOPT source code folder. It is where you untar 
+# the source code or cloned from the github repo
+# https://github.com/coin-or/Ipopt
 
-# I personally do not need support for lib64 but I'm keeping it in the code.
-def get_ipopt_lib():
-    for lib_suffix in ('lib', 'lib64'):
-        d = os.path.join(IPOPT_DIR, lib_suffix)
-        if os.path.isdir(d):
-            return d
+# The folder to find IpStdCInterface.h.
+# Default to the installation path when you run `make install`
+# for Ipopt.
+####### YOU MAY NEED TO CHANGE THIS ######## 
+IPOPT_HEADER_DIR = '/usr/local/include/coin-or/'
+####### YOU MAY NEED TO CHANGE THIS ######## 
 
-IPOPT_LIB = get_ipopt_lib()
-if IPOPT_LIB is None:
-    raise Exception('failed to find ipopt lib')
+# The folder to find libipopt.so/la.
+# Default to the install path for ipopt
+####### YOU MAY NEED TO CHANGE THIS ######## 
+IPOPT_LIB_DIR = '/usr/local/lib/'
+####### YOU MAY NEED TO CHANGE THIS ######## 
 
-IPOPT_INC = os.path.join(IPOPT_DIR, 'include/coin/')
+def get_numpy_include_dir():
+    import numpy
+    return numpy.get_include()
 
-FILES = ['src/callback.c', 'src/pyipoptcoremodule.c']
+def get_ipopt_include_dir():
+    return IPOPT_HEADER_DIR
 
-# The extra_link_args is commented out here;
-# that line was causing my pyipopt install to not work.
-# Also I am using coinmumps instead of coinhsl.
+def get_ipopt_lib_dir():
+    return IPOPT_LIB_DIR
+
+source = ['src/callback.c', 'src/pyipoptcoremodule.c']
+
+print(get_numpy_include_dir())
+print(get_ipopt_include_dir())
+
+
 pyipopt_extension = Extension(
-        'pyipoptcore',
-        FILES,
-        #extra_link_args=['-Wl,--rpath','-Wl,'+ IPOPT_LIB],
-        library_dirs=[IPOPT_LIB],
-        libraries=[
-            'ipopt', 'coinblas',
-            #'coinhsl',
-            'coinmumps',
-            'coinmetis',
-            'coinlapack','dl','m',
-            ],
-        include_dirs=[numpy_include, IPOPT_INC],
-        )
+    'pyipoptcore',
+    source,
+    library_dirs=[get_ipopt_lib_dir()],
+    libraries=[
+        'ipopt', 
+        'dl',
+        'm',
+    ],
+    include_dirs=[get_numpy_include_dir(), get_ipopt_include_dir()],
+)
 
 setup(
-        name="pyipopt",
-        version="0.8",
-        description="An IPOPT connector for Python",
-        author="Eric Xu",
-        author_email="xu.mathena@gmail.com",
-        url="https://github.com/xuy/pyipopt",
-        packages=['pyipopt'],
-        package_dir={'pyipopt' : 'pyipoptpackage'},
-        ext_package='pyipopt',
-        ext_modules=[pyipopt_extension],
-        )
-
+    name="pyipopt",
+    version="0.8",
+    description="A Python connector for Ipopt",
+    author="Eric Xu",
+    author_email="xu.mathena@gmail.com",
+    url="https://github.com/xuy/pyipopt",
+    packages=['pyipopt'],
+    package_dir={'pyipopt' : 'pyipoptpackage'},
+    ext_package='pyipopt',
+    ext_modules=[pyipopt_extension],
+)
